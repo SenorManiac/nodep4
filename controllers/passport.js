583a58ffc3bcac4b872dc9a16d827d688da85201
unknown <kyle@onlygradpas.com>
2025-01-18 15:07:46 -0500

    update

--- controllers/passport.js
+++ controllers/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../db/manageUsers'); 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 
